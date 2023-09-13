dafb6c9e0d417741376ed1050fc1351c770c40a8
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-09-14 02:55:53 +0300

    Initial Commit

---
+++ PolarTracker/PolarTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarTrackerApp.swift
+//  PolarTracker
+//
+//  Created by Batuhan Ergun on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PolarTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
