1cf0c4907e5ec48baa1fe1a1308168db14d0377e
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-09-14 07:55:38 +0300

    fix ui

--- PolarTracker/ContentView.swift
+++ PolarTracker/ContentView.swift
@@ -6,8 +6,36 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct ContentView: View {
+struct MapView: UIViewRepresentable {
+    @Binding var annotations: [MKPointAnnotation]
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.removeAnnotations(uiView.annotations)
+        uiView.addAnnotations(annotations)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+    }
+}
+
+struct DetailView: View {
     @ObservedObject var bleManager = BLEManager()
     
     var body: some View {
@@ -43,6 +71,60 @@ struct ContentView: View {
                 .padding()
             }
             .navigationBarTitle("BLE Device List")
-            }
         }
     }
+}
+
+struct MainMapView: View {
+    @State private var annotations: [MKPointAnnotation] = []
+    @State private var isShowingAnotherView = false // State to control navigation
+    @GestureState private var dragOffset = CGSize.zero // Track the drag offset
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                MapView(annotations: $annotations)
+                    .ignoresSafeArea(.all) // Make the map full screen
+                
+                // Full-width bar with a fading black background at the bottom
+                VStack {
+                    Spacer()
+                    Text("Swipe Up to Open Menu")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .padding(.top, 50)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+                .background(LinearGradient(
+                    gradient: Gradient(colors: [Color.clear, Color.black.opacity(0.7)]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                ))
+                .gesture(
+                    DragGesture()
+                        .updating($dragOffset) { value, state, _ in
+                            state = value.translation
+                        }
+                        .onEnded { value in
+                            if value.translation.height < -100 {
+                                isShowingAnotherView = true
+                            }
+                        }
+                )
+            }
+            .onAppear {
+                // Initialize and update your annotations here
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
+                annotation.title = "San Francisco"
+                annotations.append(annotation)
+            }
+            .sheet(isPresented: $isShowingAnotherView) {
+                DetailView()
+            }
+        }
+        .navigationBarTitle("", displayMode: .inline)
+        .navigationBarHidden(true) // Hide navigation bar
+    }
+}
