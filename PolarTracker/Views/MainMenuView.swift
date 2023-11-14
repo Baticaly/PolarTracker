fb1f8e91e2fc992847b7b41e8e35f83fb4e40e96
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-11-14 06:48:58 +0300

    main menu update

--- PolarTracker/Views/MainMenuView.swift
+++ PolarTracker/Views/MainMenuView.swift
@@ -13,37 +13,28 @@ struct MainMenuView: View {
 
     var body: some View {
         NavigationView {
-            List {
-                NavigationLink(destination: ConnectDeviceView()) {
-                    CardView(imageName: "rectangle.connected.to.line.below", title: "Connect", description: "Connect to a BLE device.")
+            VStack {
+                DeviceStatusView(bleManager: bleManager)
+                ScrollView {
+                    NavigationLink(destination: ConnectDeviceView()) {
+                        CardView(imageName: "antenna.radiowaves.left.and.right", title: "Connect Device", description: "Connect to a BLE device.")
+                    }
+                    .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
+                    NavigationLink(destination: ShowLocationView(sessions: bleManager.sessionHandler.sessions, mapViewModel: mapViewModel)) {
+                        CardView(imageName: "map", title: "Live Map", description: "Show the current location.")
+                    }
+                    .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
+                    NavigationLink(destination: ShowDetailsView()) {
+                        CardView(imageName: "doc.text.viewfinder", title: "Overview", description: "Show packet details")
+                    }
+                    .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
+                    NavigationLink(destination: SessionListView().environmentObject(bleManager)) {
+                        CardView(imageName: "clock.arrow.circlepath", title: "Saved Sessions", description: "Show previously saved sessions.")
+                    }
+                    .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
+                    Spacer()
                 }
-                .listRowInsets(EdgeInsets())
-                NavigationLink(destination: ShowLocationView(sessions: bleManager.sessionHandler.sessions, mapViewModel: mapViewModel)) {
-                    CardView(imageName: "map.fill", title: "Map", description: "Show the current location.")
-                }
-                .listRowInsets(EdgeInsets())
-                NavigationLink(destination: ShowDetailsView()) {
-                    CardView(imageName: "info.circle.fill", title: "Details", description: "Show detailed information.")
-                }
-                .listRowInsets(EdgeInsets())
-                NavigationLink(destination: SessionListView().environmentObject(bleManager)) {
-                    CardView(imageName: "chart.xyaxis.line", title: "Saved Sessions", description: "Show previously saved sessions.")
-                }
-                .listRowInsets(EdgeInsets())
             }
-            .navigationBarItems(leading: HStack {
-                if bleManager.isConnected {
-                    Image(systemName: "circle.fill")
-                        .foregroundColor(.red)
-                    Text("Recording")
-                }
-            }, trailing: HStack {
-                if bleManager.isConnected {
-                    Text(bleManager.connectedPeripheralName ?? "Unnamed")
-                } else {
-                    Text("Disconnected")
-                }
-            })
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
@@ -55,25 +46,82 @@ struct CardView: View {
     let description: String
 
     var body: some View {
-        ZStack(alignment: .bottom) {
-            VStack(alignment: .leading) {
-                HStack {
+        ZStack(alignment: .center) {
+            HStack {
+                VStack(alignment: .leading) {
                     Text(title)
-                        .font(.title)
+                        .font(.headline) // Adjusted font size
                         .fontWeight(.bold)
-                    Spacer()
-                    Image(systemName: imageName)
-                        .resizable()
-                        .frame(width: 40, height: 40)
+                        .foregroundColor(.white)
+                    Text(description)
+                        .font(.subheadline) // Adjusted font size
+                        .foregroundColor(.gray)
                 }
-                Text(description)
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
                 Spacer()
+                Image(systemName: imageName)
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(.white)
             }
-            .padding()
+            .padding(.top, 40)
+            .padding(.bottom, 40)
+            .padding(.leading, 35)
+            .padding(.trailing, 35)
             .frame(maxWidth: .infinity)
+            .background(Color(UIColor(hex: "#111111"))) // Dark gray background for better contrast with white text
             .cornerRadius(10)
         }
+    }
+}
+
+struct DeviceStatusView: View {
+    @ObservedObject var bleManager: BLEManager
+
+    var body: some View {
+        HStack {
+            Image("deviceIllustration")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+                .padding(.all, 10)
+
+            VStack(alignment: .leading) {
+                Text(bleManager.connectedPeripheralName ?? "Base Station 01")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                HStack {
+                    Circle()
+                        .fill(bleManager.isConnected ? Color.green : Color.gray)
+                        .frame(width: 10, height: 10)
+                    Text(bleManager.isConnected ? "Recording" : "Offline")
+                        .font(.subheadline)
+                }
+            }
+            .padding(.all, 3)
+            Spacer()
+        }
+        .padding()
+        .background(Color(UIColor(hex: "#1c1c1b")))
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.currentIndex = hex.hasPrefix("#") ? hex.index(after: hex.startIndex) : hex.startIndex
+
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, 
+            alpha: 1
+        )
     }
 }
